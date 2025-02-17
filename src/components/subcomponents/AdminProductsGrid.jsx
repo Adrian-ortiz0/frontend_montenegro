64fdca63157d49b0182c886f454c5301e45669bc
Adrian-ortiz0 <dxniel7328@gmail.com>
2025-02-16 22:09:31 -0500

    feat: :sparkles: I've added the select buttons

--- src/components/subcomponents/AdminProductsGrid.jsx
+++ src/components/subcomponents/AdminProductsGrid.jsx
@@ -1,16 +1,32 @@
 import React, { useState } from "react";
 import { Add, Loupe } from "@mui/icons-material";
-import { colors, IconButton, Menu, MenuItem } from "@mui/material";
+import {
+  colors,
+  FormControl,
+  IconButton,
+  InputLabel,
+  Menu,
+  MenuItem,
+  Select,
+} from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { blue } from "@mui/material/colors";
 
 const AdminProductsGrid = () => {
   const [anchorEl, setAnchorEl] = useState();
   const open = Boolean(anchorEl);
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  const [categoryProducts, setCategoryProducts] = useState("");
+
+  const handleChange = (e) => {
+    setCategoryProducts(e.target.value);
   };
 
   const productos = [
@@ -81,6 +97,26 @@ const AdminProductsGrid = () => {
           <button className="add_button">
             <Add className="nav_icon" />
           </button>
+          <FormControl sx={{ m: 1, minWidth: 120, backgroundColor: "#2c2c2c", color: "white", borderRadius: "15px", marginRight: 3 }} size="small">
+            <InputLabel id="demo-select-small-label" sx={{color: "#b8b8b8"}}>Category</InputLabel>
+            <Select
+              sx={{color: "#888888", borderRadius: "15px"}}
+              labelId="demo-select-small-label"
+              id="demo-select-small"
+              value={categoryProducts}
+              label="Category"
+              onChange={handleChange}
+            >
+              <MenuItem value={"all"}>
+                <em>All</em>
+              </MenuItem>
+              <MenuItem value={"pizza"}>Pizza</MenuItem>
+              <MenuItem value={"panzeroti"}>Panzzeroti</MenuItem>
+              <MenuItem value={"lasagna"}>Lasagna</MenuItem>
+              <MenuItem value={"desgranados"}>Desgranados</MenuItem>
+              <MenuItem value={"bebidas"}>Bebidas</MenuItem>
+            </Select>
+          </FormControl>
         </div>
         <div className="products">
           {productos.map((producto) => (
@@ -96,7 +132,6 @@ const AdminProductsGrid = () => {
                     top: 50,
                     right: 5,
                     color: "grey",
-
                   }}
                   onClick={(e) => {
                     e.stopPropagation();
@@ -114,8 +149,8 @@ const AdminProductsGrid = () => {
                       style: {
                         backgroundColor: "#2c2c2c",
                         color: "white",
-                      }
-                    }
+                      },
+                    },
                   }}
                 >
                   <MenuItem onClick={handleClose}>Editar</MenuItem>
